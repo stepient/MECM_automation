38e18ac24de2976d64dc5966792bf1ed47fcc47e
stepient <68950541+stepient@users.noreply.github.com>
2021-08-26 12:38:57 +0200

    Update 11_InstallReportingServices.ps1

--- MECM_Scripts/11_InstallReportingServices.ps1
+++ MECM_Scripts/11_InstallReportingServices.ps1
@@ -2,8 +2,10 @@
 $InstallPath = "D:\Program Files\SSRS"
 
 Push-Location
+
 #change install location to D:\
 Set-Location $SourceFilesLocation
 & .\SQLServerReportingServices.exe /passive /norestart /IAcceptLicenseTerms /InstallFolder=$InstallPath
+Wait-Process -Name SQLServerReportingServices
 
 Pop-Location
