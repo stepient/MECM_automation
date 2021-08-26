f3c576c110869f72bdc1963238f34183e8ec9c07
stepient <68950541+stepient@users.noreply.github.com>
2021-08-26 11:36:49 +0200

    Update 8_InstallReportViewer.ps1

--- MECM_Scripts/8_InstallReportViewer.ps1
+++ MECM_Scripts/8_InstallReportViewer.ps1
@@ -4,6 +4,7 @@ Push-Location
 Set-Location $MSILocation
 
 msiexec /passive /norestart /i SQLSysCLRTypes.msi
+Start-Sleep -Seconds 10
 msiexec /passive /norestart /i ReportViewer.msi
 
 Pop-Location
