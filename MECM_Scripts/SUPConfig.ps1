659cc665a67079265d4ec7f29e9bc2d076fb574a
Tomasz Stepien <tomasz.stepien@capgemini.com>
2021-08-27 14:41:30 +0000

    modified wsus db config script

--- MECM_Scripts/SUPConfig.ps1
+++ MECM_Scripts/SUPConfig.ps1
@@ -1,11 +1,20 @@
-﻿param(
+﻿#run it on the primary site server
+param(
+    $DBServerName = $env:COMPUTERNAME,
     $DBFilePath = "$PSScriptRoot\WSUSDBConfig.sql",
-    $LogPath = "$PSScriptRoot\Logs\WSUSDBConfig.log"
+    $LogPath = "$PSScriptRoot\Logs\WSUSDBConfig.log",
+    $WSUSDBName = "SUSDB",
+    $DBPath = "E:\SQL_Database\SUSDB.mdf",
+    $WSUSDBAutogrowth = 512
 )
 
 $SQLScript = @" 
 -- Change DB owner to sa
-ALTER AUTHORIZATION ON DATABASE::CM_$SiteCode TO sa;
+ALTER AUTHORIZATION ON DATABASE::$WSUSDBName TO sa;
+GO
+
+ALTER DATABASE $WSUSDBName
+MODIFY FILE ( NAME = $DBPath, FILEGROWTH = $WSUSDBAutogrowth )
 GO
 "@
 
