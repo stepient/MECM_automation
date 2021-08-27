bedd16a161a487d39f1295251b52521c783f7a84
Tomasz Stepien <tomasz.stepien@capgemini.com>
2021-08-27 14:42:58 +0000

    file modified

--- MECM_Scripts/SUPPreConfig_2.ps1
+++ MECM_Scripts/SUPPreConfig_2.ps1
@@ -1,21 +1,27 @@
 ﻿param(
     $SiteSystemServerFQDN = "CM01.dev.local",
     $WSUSDir = "D:\WSUS Content",
-    [int]$WSUSQueueLength = 5000,
-    [int]$WSUSMemoryLimit = 1000
+    [int]$WSUSQueueLength = 6000,
+    [int]$WSUSMemoryLimit = 0
 )
 
 $OldErrorActionPreference = $ErrorActionPreference
 $ErrorActionPreference = "Stop"
 
 Try{
-    
-    Push-Location
-    Set-Location "C:\Program Files\Update Services\Tools"
-    .\wsusutil.exe postinstall SQL_INSTANCE_NAME=$SiteSystemServerFQDN CONTENT_DIR=$WSUSDir #configure WSUS
-    Pop-Location
-
-    Install-Module WebAdministration
+    $WSUSServer = Get-WsusServer -ErrorAction SilentlyContinue
+    If(-not $WSUSServer)
+    {
+        Push-Location
+        Set-Location "C:\Program Files\Update Services\Tools"
+        .\wsusutil.exe postinstall SQL_INSTANCE_NAME=$SiteSystemServerFQDN CONTENT_DIR=$WSUSDir #configure WSUS
+        Pop-Location
+    }
+    else
+    {
+        Write-Output "WSUS server is already configured"
+    }
+    Import-Module WebAdministration
 
     #Import functions
     . "$PSScriptRoot\Functions\Set-WSUSPoolQueueLength.ps1"
@@ -29,8 +35,6 @@ Try{
     #Under Databases, Right-click SUSDB, select Properties and click Files
     #Change Owner to SA
     #Change the Autogrowth value to 512MB, click Ok and close SQL MS
-    #in IIS console change queue length to 10000
-    #adjust the private memory limit
 }
 Finally
 {
