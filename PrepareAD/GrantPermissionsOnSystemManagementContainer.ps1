6f1af0df85aa6d18bec79c0d53328bce49542912
stepient <68950541+stepient@users.noreply.github.com>
2021-09-16 16:46:22 +0200

    Update and rename GrantPermissionsOnSystemContainer.ps1 to GrantPermissionsOnSystemManagementContainer.ps1

--- PrepareAD/GrantPermissionsOnSystemManagementContainer.ps1
+++ PrepareAD/GrantPermissionsOnSystemManagementContainer.ps1
@@ -8,14 +8,14 @@ $VerbosePreference = "Continue"
 
 $root = (Get-ADRootDSE).defaultNamingContext
 
-Write-Verbose -Message "Setting permissions for object CN=System,$root"
-$acl = get-acl "ad:CN=System,$root"
+Write-Verbose -Message "Setting permissions for object CN=System Management,CN=System,$root"
+$acl = get-acl "ad:CN=System Management,CN=System,$root"
 
 Try{
     $ComputerObj = Get-ADComputer $MECMSiteServer
     if ($ComputerObj -eq $null)
     {
-        Write-Verbose -Message "Unable to find computer $MECMServersADGroup"
+        Write-Verbose -Message "Unable to find computer $MECMSiteServer"
     }
     else
     {
@@ -24,7 +24,7 @@ Try{
         $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $ComputerObj.SID, "GenericAll", "Allow", $All
 
         $acl.AddAccessRule($ace) 
-        Set-acl -aclobject $acl "ad:CN=System,$root"
+        Set-acl -aclobject $acl "ad:CN=System Management,CN=System,$root"
         Write-Verbose -Message "Permissions on the SYSTEM container sucessfully granted to computer account $MECMSiteServer"
     }
 }
