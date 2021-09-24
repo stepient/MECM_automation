89bce4492d00b0a5bba2e8d99cc9f70a7a9716a7
stepient <68950541+stepient@users.noreply.github.com>
2021-09-24 14:46:22 +0200

    Update ImportMECMServersGPO.ps1
    Added the migration table feature

--- PrepareAD/ImportMECMServersGPO.ps1
+++ PrepareAD/ImportMECMServersGPO.ps1
@@ -3,8 +3,14 @@ param(
     $ServersOUPath = "OU=Servers" + "," + $MECMOUPath,
     $NewGPOName = "MECM_Servers",
     $BackupGPOName = "MECM_Servers",
-    $GPOBackupPath = "$PSScriptRoot\GPO"
+    $GPOBackupPath = "$PSScriptRoot\GPO",
+    $SecPrincipalsCSVPath = "$PSScriptRoot\InputFiles\SecurityPrincipals.csv",
+    $MigTableSource = "$PSScriptRoot\MigTableTemplate.migtable",
+    $MigrationTable = "$PSScriptRoot\MigTable.migtable"
 )
+
+$OldVerbosePreference = $VerbosePreference
+$VerbosePreference = "Continue"
 
 $params = @{
 
@@ -13,11 +19,56 @@ $params = @{
     TargetName = $NewGPOName
     CreateIfNeeded = $true
     ErrorAction = 'Stop'
+    MigrationTable = $MigrationTable
 }
 
-$OldVerbosePreference = $VerbosePreference
-$VerbosePreference = "Continue"
 #Create a migration table
+$SecPrincipalIDs = @(
+    @{
+        Name = "MECM-SQLService"
+        ID = 0
+    }
+
+    @{
+        Name = "MECM-SQLReporting"
+        ID = 1
+    }
+
+    @{
+        Name = "MECM-DomainJoin"
+        ID = 2
+    }
+
+    @{
+        Name = "MECM-ClientPush"
+        ID = 3
+    }
+
+    @{
+        Name = "MECM-Admins"
+        ID = 4
+    }
+
+    @{
+        Name = "MECM-Servers"
+        ID = 5
+    }
+)
+
+$SecPrincipals = Import-Csv $SecPrincipalsCSVPath -Delimiter ','
+$DomainName = (Get-ADDomain).DnsRoot
+
+[xml]$MigTable = Get-Content $MigTableSource
+$SecPrincipalsMigTable = $MigTable.MigrationTable.Mapping.Source
+
+Foreach ($SecPrincipal in $SecPrincipalIDs)
+{
+    
+    ($MigTable.MigrationTable.Mapping | where {$_.Source -eq $SecPrincipal.Name}).Destination = $($DomainName + "\" + $($SecPrincipals | where {$_.ID -eq $SecPrincipal.ID}).Name)
+}
+
+$MigTable.Save($MigrationTable)
+
 Remove-Variable ExistingGPO -ErrorAction SilentlyContinue
 
 Try{
