0c5b0116967704ac87f3c9f6bf6515b49b265257
stepient <68950541+stepient@users.noreply.github.com>
2022-02-21 12:39:19 +0100

    Create _Configure_Boundaries.ps1

---
+++ MECM_Scripts/_Configure_Boundaries.ps1
@@ -0,0 +1,39 @@
+#
+# Press 'F5' to run this script. Running this script will load the ConfigurationManager
+# module for Windows PowerShell and will connect to the site.
+#
+# This script was auto-generated at '2/21/2022 11:24:37 AM'.
+
+# Uncomment the line below if running in an environment where script signing is 
+# required.
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
+# Site configuration
+$SiteCode = "LAB" # Site code 
+$ProviderMachineName = "CM01.test.local" # SMS Provider machine name
+
+# Customizations
+$initParams = @{}
+#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Do not change anything below this line
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
+
+New-CMBoundary -Name Default -Type ADSite -Value Default-First-Site-Name
+
+New-CMBoundaryGroup -AddSiteSystemServerName $ProviderMachineName -DefaultSiteCode $SiteCode -Name Default
+
+Add-CMBoundaryToGroup -BoundaryGroupName Default -BoundaryName Default
